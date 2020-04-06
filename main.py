c0e05a311621496b899e9573ad90494c3ee3c7da
Daniel George Partridge <daniel1@daniels-mbp-2.home>
2020-04-06 12:48:37 +0100

    females now randomly give birth

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import time
 from names import AllNames
 
-max_sims = 10
+max_sims = 25
 basic_info = ['gender', 'name', 'age']
 genders = ('boy', 'girl')
 
@@ -22,7 +22,8 @@ class Simulation:
 			self.igt()
 
 			if random.random() < spawn_pc and len(self.sims) <= max_sims:
-				Sim()
+				sim = Sim()
+				sim.generate()
 			elif len(self.sims) >= max_sims:
 				self.running = False
 
@@ -30,11 +31,28 @@ class Simulation:
 		for sim in self.sims:
 			sim.info['age'][1]['days_to_age_up'] -= 1
 
-			if sim.info['age'][1]['days_to_age_up'] < 0 and sim.info['age'][0] <= 6:
+			if sim.info['age'][1]['days_to_age_up'] < 0:
 				sim.info['age'][0] += 1
-				sim.add_to_info('age', sim.age_up())
-			elif sim.info['age'][0] > 6:
-				self.sims.remove(sim)
+				if sim.info['age'][0] > 6:
+					self.sims.remove(sim)
+					print(f'{sim.first_name} {sim.surname} died!')
+				else:
+					sim.add_to_info('age', sim.age_up())
+
+			print(sim.info['name'], sim.info['age'][1]['group'], sim.info['gender'])
+
+	def sim_births(self):
+		spawn_pc = 0.05
+
+		for sim in self.sims:
+			if random.random() < spawn_pc and sim.info['gender'] == 'girl' and sim.info['age'][0] >= 3:
+				child = Offspring(sim)
+				child.generate()
+				sim.offspring.append(child)
+
+				print(f'{sim.first_name} {sim.surname} gave birth to {child.first_name} {child.surname}!')
+
+				time.sleep(2)
 
 	def igt(self):
 		self.step += 1
@@ -43,8 +61,9 @@ class Simulation:
 			self.day += 1
 			self.step = 0
 			self.sim_aging()
-			print(self.day)
-			print([sim.info for sim in self.sims])
+			self.sim_births()
+			#print(self.day)
+			#print([sim.info for sim in self.sims])
 
 		time.sleep(0.2)
 
@@ -61,20 +80,26 @@ class Sim:
 				  	 3: {'group': 'teen', 'days_to_age_up': 21},
 				  	 4: {'group': 'yng_adult', 'days_to_age_up': 21},
 				  	 5: {'group': 'adult', 'days_to_age_up': 60},
-				  	 6: {'group': 'elder', 'days_to_age_up': 28},
+				  	 6: {'group': 'elder', 'days_to_age_up': 2},
 				    }
+		self.offspring = []
+
+	def generate(self):
+		self.properties = self.set_gender, self.set_name, self.set_age
 		self.set_basic_info()
+		self.first_name, self.surname = self.info['name'][0], self.info['name'][1]
+
+		print(f'{self.first_name} {self.surname} spawned! {self.info["age"]}')
 		s.sims.append(self)
 	
 	def set_basic_info(self):
-		properties = self.set_gender, self.set_name, self.set_age
-		for func in properties:
+		for func in self.properties:
 			for item in basic_info:
 				if str(item) in str(func):
 					self.add_to_info(item, func())
 
 	def set_name(self):
-		return random.choice(n.first_names[self.info['gender']]) + " " + random.choice(n.surnames)
+		return [random.choice(n.first_names[self.info['gender']]), random.choice(n.surnames)]
 
 	def set_gender(self):
 		return random.choice(genders)
@@ -88,6 +113,18 @@ class Sim:
 	def add_to_info(self, prop, val):
 		self.info.update({prop: val})
 
+
+class Offspring(Sim):
+	def __init__(self, mother):
+		self.mother = mother
+		super().__init__()
+		
+	def set_age(self):
+		first_age = list(self.ages.keys())[0]
+		return [first_age, {key: value for key, value in self.ages[first_age].items()}]
+
+	def set_name(self):
+		return [random.choice(n.first_names[self.info['gender']]), self.mother.surname]
 
 if __name__ == "__main__":
 	s.main_loop()
